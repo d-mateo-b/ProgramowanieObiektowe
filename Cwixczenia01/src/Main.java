f90d82389ce332e7f4dc540bcea487351c9ef186
Mateusz Dyek <mateuszdymek1202@gmail.com>
2026-09-11 09:42:29 +0200

    Instrukcje wypisania

--- Cwixczenia01/src/Main.java
+++ Cwixczenia01/src/Main.java
@@ -17,5 +17,22 @@ void main() {
     System.out.println("\'apostrof\'");
 
     System.out.println("backslash:\\");
+
+    int LiczbaCalkowita = 5;
+    double LiczbaReczywista;
+
+    LiczbaReczywista = (double) LiczbaCalkowita /2;
+    System.out.println("Wynik dzielenia liczby 5 przez 2 to " + LiczbaReczywista);
+
+    char znak = 'a';
+    System.out.println("Litera a ma wartość " + (znak + 0));
+
+    String napis = "napis";
+    System.out.println("Napis wielkimi literami: " + napis.toUpperCase());
+
+    System.out.println("Wynik 1 + 2 + 3 = " + 1 + 2 + 3);
+    System.out.println("Wynik 1 + 2 + 3 = " +( 1 + 2 + 3));
+    System.out.println(1 + 2 + 3" to Wynik 1 + 2 + 3");
+
 }
 

939c0495f6d4dd07f0fc0c4f0c12cf1b70fe2cd8
Mateusz Dymek <mateusz@gmail.com>
2026-09-24 11:29:15 +0200

    zad02

--- zadania02/src/Main.java
+++ zadania02/src/Main.java
@@ -47,29 +47,113 @@ void main() {
 
 
 //zad3
+//    Scanner scanner = new Scanner(System.in);
+//
+//    System.out.print("Podaj pierwszą liczbę: ");
+//    double liczba1 = scanner.nextDouble();
+//
+//    System.out.print("Podaj drugą liczbę: ");
+//    double liczba2 = scanner.nextDouble();
+//
+//    if (liczba1 > liczba2) {
+//        System.out.println("Największa liczba to: " + liczba1);
+//    } else if (liczba2 > liczba1) {
+//        System.out.println("Największa liczba to: " + liczba2);
+//    } else {
+//        System.out.println("Obie liczby są równe.");
+//    }
+//
+//    scanner.close();
+    //zad5
 
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Podaj trzy długości boków:");
 
-    System.out.print("Bok a: ");
-    double a = scanner.nextDouble();
+    System.out.print("Podaj numer miesiąca (1-12): ");
+    int numer = scanner.nextInt();
 
-    System.out.print("Bok b: ");
-   double b = scanner.nextDouble();
+    switch (numer) {
+        case 1:
+            System.out.println("Styczeń");
+            break;
+        case 2:
+            System.out.println("Luty");
+            break;
+        case 3:
+            System.out.println("Marzec");
+            break;
+        case 4:
+            System.out.println("Kwiecień");
+            break;
+        case 5:
+            System.out.println("Maj");
+            break;
+        case 6:
+            System.out.println("Czerwiec");
+            break;
+        case 7:
+            System.out.println("Lipiec");
+            break;
+        case 8:
+            System.out.println("Sierpień");
+            break;
+        case 9:
+            System.out.println("Wrzesień");
+            break;
+        case 10:
+            System.out.println("Październik");
+            break;
+        case 11:
+            System.out.println("Listopad");
+            break;
+        case 12:
+            System.out.println("Grudzień");
+            break;
+        default:
+            System.out.println("Nieprawidlowy numer miesiaca");
+            break;
 
-    System.out.print("Bok c: ");
-    double c = scanner.nextDouble();
-
-
-    boolean czyMoznaZbudowac = (a + b > c) && (a + c > b) && (b + c > a) && (a > 0) && (b > 0) && (c > 0);
-
-  if (czyMoznaZbudowac) {
-       System.out.println("Tak, z podanych boków można zbudować trójkąt.");
-   } else {
-        System.out.println("Nie, z podanych boków nie można zbudować trójkąta.");
     }
 
     scanner.close();
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
 

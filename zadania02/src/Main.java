2bfa30956bc6d4c74b596558097155af963da3df
Mateusz Dymek <mateusz@gmail.com>
2026-09-25 09:38:09 +0200

    zad02

--- zadania02/src/Main.java
+++ zadania02/src/Main.java
@@ -65,73 +65,116 @@ void main() {
 //
 //    scanner.close();
     //zad5
+//
+//    Scanner scanner = new Scanner(System.in);
+//
+//    System.out.print("Podaj numer miesiąca (1-12): ");
+//    int numer = scanner.nextInt();
+//
+//    switch (numer) {
+//        case 1:
+//            System.out.println("Styczeń");
+//            break;
+//        case 2:
+//            System.out.println("Luty");
+//            break;
+//        case 3:
+//            System.out.println("Marzec");
+//            break;
+//        case 4:
+//            System.out.println("Kwiecień");
+//            break;
+//        case 5:
+//            System.out.println("Maj");
+//            break;
+//        case 6:
+//            System.out.println("Czerwiec");
+//            break;
+//        case 7:
+//            System.out.println("Lipiec");
+//            break;
+//        case 8:
+//            System.out.println("Sierpień");
+//            break;
+//        case 9:
+//            System.out.println("Wrzesień");
+//            break;
+//        case 10:
+//            System.out.println("Październik");
+//            break;
+//        case 11:
+//            System.out.println("Listopad");
+//            break;
+//        case 12:
+//            System.out.println("Grudzień");
+//            break;
+//        default:
+//            System.out.println("Nieprawidlowy numer miesiaca");
+//            break;
+//
+//    }
+//
+//    scanner.close();
+//
 
-    Scanner scanner = new Scanner(System.in);
-
-    System.out.print("Podaj numer miesiąca (1-12): ");
-    int numer = scanner.nextInt();
-
-    switch (numer) {
-        case 1:
-            System.out.println("Styczeń");
-            break;
-        case 2:
-            System.out.println("Luty");
-            break;
-        case 3:
-            System.out.println("Marzec");
-            break;
-        case 4:
-            System.out.println("Kwiecień");
-            break;
-        case 5:
-            System.out.println("Maj");
-            break;
-        case 6:
-            System.out.println("Czerwiec");
-            break;
-        case 7:
-            System.out.println("Lipiec");
-            break;
-        case 8:
-            System.out.println("Sierpień");
-            break;
-        case 9:
-            System.out.println("Wrzesień");
-            break;
-        case 10:
-            System.out.println("Październik");
-            break;
-        case 11:
-            System.out.println("Listopad");
-            break;
-        case 12:
-            System.out.println("Grudzień");
-            break;
-        default:
-            System.out.println("Nieprawidlowy numer miesiaca");
-            break;
-
-    }
-
-    scanner.close();
+//zad6
+//
+//    String imie = "Mateusz";
+//
+//    Scanner scanner = new Scanner(System.in);
+//
+//    System.out.println("Podaj swoje imie");
+//
+//
+//    String nazwa = scanner.nextLine();
+//
+//    if (imie.equalsIgnoreCase(nazwa)) {
+//        System.out.println("Masz tak samo na imie jak ja" + imie  + ".");
+//    }else {
+//        System.out.println("Mamy różne imiona " + nazwa + ", a ja to " + imie + ".");
+//    }
+//
+//
+//
+//
+//scanner.close();
+//
 
 
+//zad7
+//
+//    Scanner scanner = new Scanner(System.in);
+//
+//    System.out.println("Podaj swój wiek");
+//
+//    int wiek = scanner.nextInt();
+//
+//    boolean Czypelnoletni =  (wiek  >= 18) ? true : false ;
+//
+//    System.out.println("Czy użtkownik jest pełnoletni  " + Czypelnoletni);
+//
+//    scanner.close();
 
 
+    //zad8
+//
+//Scanner scanner = new Scanner(System.in);
+//
+//    System.out.println("Podaj aktualny rok");
+//
+//    int rok = scanner.nextInt();
+//
+//    if ((rok % 4 == 0 && rok % 100 != 0 ) || (rok % 400 == 0)){
+//        System.out.println( rok +" rok jest przestepny ");
+//    }else{
+//        System.out.println( rok +" rok nie jest przestepny ");
+//    }
+//
+//scanner.close();
+//
+//zad9
 
-
-
-
-
-
-
-
-
-
-
-
-
+    
 
 
 
